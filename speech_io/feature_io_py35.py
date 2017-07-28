f4f9514a96729908734e4ac9e5a319e1f7c25aaa
Austin Zhang <ZhangAustin@users.noreply.github.com>
2017-07-27 17:01:22 -0700

    bug fix and faster for python 3.5

--- speech_io/feature_io_py35.py
+++ speech_io/feature_io_py35.py
@@ -1,15 +1,28 @@
+"""
+Read and write HTK feature files.
+This module reads and writes the acoustic feature files used by HTK
 
-# coding: utf-8
+Check up-to-date 
+https://github.com/AustinZhang/deep-speech
 
-# In[ ]:
+Usage: from speech_io.feature_io import *
 
+OBS = read_scp(r'\\vilfblgpu020\D\users\zhashi\BNFeat\scps\BNDNN_BN_1300hr.mfc.head.fakechunk')
+
+
+Author:         zhashi@microsoft.com
+Last update:    2015/12
+
+Bug fixed: self.filesize --> self.nsamples in the HTK header
+
+"""
 
 
 from struct import unpack, pack
 import numpy
 
-__author__ = 'Shixiong Zhang, Zhuo Chen'
-__version__ = "1.0.2"
+__author__ = 'Shixiong Zhang'
+__version__ = "1.0.3"
 
 # adapt to python3
 LPC = 1
@@ -133,7 +146,7 @@ class HTKFeat_read(object):
         return data
 
     def getchunk(self, startframe,
-                 endframe):  # zhashi: support for chunkread   # \\fbl\NAS\INVES\ruizhao\DNN\sequential\smdr3train\chunk\archive.0[296,735]
+                 endframe):  # zhashi: support for chunkread   # .\chunk\archive.0[296,735]
         self.seek(startframe)
         data = numpy.fromfile(self.fh, self.dtype, (endframe - startframe + 1) * self.veclen)
 
@@ -147,7 +160,7 @@ class HTKFeat_read(object):
 
 
 class HTKFeat_write(object):
-    "Write Sphinx-II format feature files"
+    "Write HTK format feature files"
 
     def __init__(self, filename=None,
                  veclen=13, sampPeriod=100000,
@@ -157,7 +170,7 @@ class HTKFeat_write(object):
         self.sampSize = veclen * 4
         self.paramKind = paramKind
         self.dtype = 'f'
-        self.filesize = 0
+        self.nsamples = 0
         self.swap = (unpack('=i', pack('>i', 42))[0] != 42)
         if (filename != None):
             self.open1(filename)
@@ -175,7 +188,7 @@ class HTKFeat_write(object):
 
     def writeheader(self):
         self.fh.seek(0, 0)
-        self.fh.write(pack(">IIHH", self.filesize,
+        self.fh.write(pack(">IIHH", self.nsamples,
                            self.sampPeriod,
                            self.sampSize,
                            self.paramKind))
@@ -187,10 +200,13 @@ class HTKFeat_write(object):
             numpy.array(vec, self.dtype).byteswap().tofile(self.fh)
         else:
             numpy.array(vec, self.dtype).tofile(self.fh)
-        self.filesize = self.filesize + self.veclen
+        self.nsamples = self.nsamples + 1 #self.veclen
 
     def writeall(self, arr):
-        for row in arr:
+        # fix a spical case that arr only has 1 frame
+        if arr.ndim==1:
+            arr=[arr]            
+        for row in arr:            
             self.writevec(row)
 
 
@@ -204,21 +220,40 @@ class HTKFeat_write(object):
 #     #frame_num, feat_dim = bnfea.shape
 #     return obs
 
+def write_one_mfc(filename, numpy_matrix):    
+    if numpy_matrix.ndim==2:
+        frame_num, fea_dim = numpy_matrix.shape           
+    if numpy_matrix.ndim==1:
+        fea_dim = numpy_matrix.shape[-1]        
+
+    mfc = hopen(filename, 'wb', fea_dim)    
+    mfc.writeall(numpy_matrix)
 
 def read_one_mfc(filename, startframe=0, endframe=0):
-    # """ read one mfcc file using htkmfc.py"""
+    # """ read one mfcc file using htkmfc.py"""        
     mfc = hopen(filename)  # <htkmfc.HTKFeat_read object at 0x6ffffae1e90>  \\fbl\NAS\INVES\ruizhao\DNN\sequential\smdr3train\chunk\archive.0
     if (startframe == endframe == 0):
         obs = mfc.getall()  # or getchunk(startframe, endframe)  or getall()
     elif (endframe >= startframe):
         obs = mfc.getchunk(startframe, endframe)  # or getchunk(startframe, endframe)  or getall()
     else:
-        print( "startframe must be less than or equal to endframe")
+        print("startframe must be less than or equal to endframe")
+    # frame_num, feat_dim = bnfea.shape
+    return obs,mfc
+
+def read_one_mfc_fast(mfc, startframe=0, endframe=0):
+    # """ read one mfcc file using htkmfc.py"""            
+    if (startframe == endframe == 0):
+        obs = mfc.getall()  # or getchunk(startframe, endframe)  or getall()
+    elif (endframe >= startframe):
+        obs = mfc.getchunk(startframe, endframe)  # or getchunk(startframe, endframe)  or getall()
+    else:
+        print("startframe must be less than or equal to endframe")
     # frame_num, feat_dim = bnfea.shape
     return obs
 
 
-def readscp(filename):
+def read_scp(filename):
     """Read scp files and the corresponding mfc files, Converted it to a dictionary"""
 
 
@@ -226,25 +261,28 @@ def readscp(filename):
     check_format = 0
     utt_dict = {}
 
+    mfc_file_prev = 'XXXXXXYYY.mfc'
+    mfcfp = None
     for line in file(filename):
         # Mobile_VS_Train_en-US_Live_06-2013/None/3cccdf83-49cb-4f92-bd96-ac1b63d9d553/7b79fb7e-ccc4-11e2-a953-001517a366d9.mfc=\\vilfblgpu020\D\users\zhashi\BNFeat\BNDNN_BN_1300hr\7b79fb7e-ccc4-11e2-a953-001517a366d9.mfc[0,489]
 
         startframe = 0
         endframe = 0
         if line.find('#'): line = line[:line.find('#')]  # Get rid of comments.
-        line = line.split()[0]  # If the line is empty, who cares?
+        line = line.split()[0]  # If the line is empty, who cares?        
         if not line: continue
 
         if '=' in line:
             set_file_name, line = line.split('=')
             check_format = 1
         if '[' in line:
+
             line, rest = line.split('[')
             startframe = int(rest.split(',')[0])
             endframe = int(rest.split(',')[1].split(']')[0])
-
-        mfc_file = line  # <------------- be careful, hard code for TIMIT
-        # print "processing", mfc_file
+        
+        mfc_file = line  # <------------- be careful, hard code for TIMIT        
+        #print "processing", mfc_file
 
         # for os in comp.Win32_OperatingSystem():
         #    print float(os.FreePhysicalMemory)/1000, "KB of available memory"
@@ -255,12 +293,18 @@ def readscp(filename):
         #    break
         # else:
 
-        obs = read_one_mfc(mfc_file, startframe, endframe)
+
+        if (mfc_file == mfc_file_prev):
+            obs = read_one_mfc_fast(mfcfp, startframe, endframe)            
+        else:
+            #mfcfp.close() # in python, file will be close automatically when run out of scope
+            obs, mfcfp = read_one_mfc(mfc_file, startframe, endframe)  # zhashi: new archive file in the scp list
+        mfc_file_prev =mfc_file
 
         if check_format:
             pure_name = set_file_name.split('.')[0]
         else:
-            pure_name = mfc_file.split('\\')[-1][:-4]
+            pure_name = mfc_file.split('\\')[-1].split('.')[0] #.split('\\')[-1][:-4]            
         # print "O_purename:", pure_name
 
         # Create and utterance dictionary to filter the MLF loading
@@ -268,9 +312,9 @@ def readscp(filename):
             utt_dict[pure_name] = 1
         o_utt[pure_name] = obs
 
+
     return o_utt, utt_dict
 
-    # OLD format
     # key = Mobile_SMD_en-US_Live_R3/44340/44340/45940
     # o_utt[key]=[[  3.84167051e+00   2.94051337e+00   4.03393555e+00 ...,   3.89492586e-02
                  #   4.45630476e-02  -4.89231683e-02]
@@ -285,4 +329,3 @@ def readscp(filename):
                  #  -2.57759001e-02  -2.50774412e-03]
                  #[  4.76648808e+00   2.39261246e+00   3.40049219e+00 ...,  -1.77562032e-02
                  #  -2.36367565e-02  -4.42066835e-03]]
-
